c8e50d814e5b9ec1fee0c419eff189695e25963d
DampfFarn <145361017+DampfFarn@users.noreply.github.com>
2024-12-10 20:51:35 +0100

    ...
    BulletPrefab
    Schießen
    GameOverScreen
    Paralax

--- Assets/Codes/Parallaxspawner.cs
+++ Assets/Codes/Parallaxspawner.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Parallaxspawner : MonoBehaviour
 {
+    private float startPos;
+    private float lenght;
+    public GameObject cam;
+    public float parallaxEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + lenght)
+        {
+            startPos += lenght;
+        }
+
+        else if (movement < startPos - lenght)
+        {
+            startPos -= lenght;
+        }
     }
 }
+
